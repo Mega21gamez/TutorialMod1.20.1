86d87993511dd48be8694cc8dcf9005a878b308d
Mega_21 <billyminchew13@gmail.com>
2025-05-14 04:01:29 +0100

    DOES NOT CURRENTLY WORK transferred checking to tags

--- src/main/java/net/mega/tutorial/block/custom/MagicBlock.java
+++ src/main/java/net/mega/tutorial/block/custom/MagicBlock.java
@@ -1,6 +1,7 @@
 package net.mega.tutorial.block.custom;
 
 import net.mega.tutorial.item.ModItems;
+import net.mega.tutorial.util.ModTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.item.TooltipContext;
@@ -36,12 +37,16 @@ public class MagicBlock extends Block {
     @Override
     public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
         if(entity instanceof ItemEntity itemEntity) {
-            if(itemEntity.getStack().getItem() == ModItems.RAW_PINK_GARNET) {
+            if(isValidItem(itemEntity.getStack())) {
                 itemEntity.setStack(new ItemStack(Items.DIAMOND, itemEntity.getStack().getCount()));
             }
         }
 
         super.onSteppedOn(world, pos, state, entity);
+    }
+
+    private boolean isValidItem(ItemStack stack) {
+        return stack.isIn(ModTags.Items.TRANSFORMABLE_ITEMS);
     }
 
     @Override
